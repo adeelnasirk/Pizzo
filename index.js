bec5c9584f716a7ff468963d529fa43e0b748a14
Adeel Nasir <adeelnasirk4@gmail.com>
2025-06-01 11:15:18 +0500

    gsap update

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var tl = gsap.timeline()
 
-tl.to('.box',{rotation:360, duration:3})
-tl.to('.box',{x:0, y:0, duration:2 , ease: "bounce.out"},1)
+tl.to('.box',{rotation:360, duration:1.5})
+tl.to('.box',{x:0, y:0, duration:1.2 , ease: "bounce.out"},1)
 
-tl.from('.nav-item', {y:-100, opacity:0, duration:1, stagger:0.3})
-tl.from('.hero-item', {x:-100, opacity:0, duration:1, stagger:0.3})
+tl.from('.nav-item', {y:-100, opacity:0, duration:1, stagger:0.1})
+tl.from('.hero-item', {x:-100, opacity:0, duration:1, stagger:0.1})
   
 tl.to(".delivery-tag", {
     opacity:1,
@@ -15,38 +15,40 @@ tl.to(".delivery-tag", {
   });
 
 
-tl.to('.box',{
+var tl2 = gsap.timeline({
   scrollTrigger:{
-    trigger:'.hero',
-    start:'90% 70%',
-    end:'150% 50%',
+    trigger:'.section-2',
+    start:'0% 90%',
+    end:'50% 50%',
     scrub:true,
-  },
+  }
+})
+
+tl2.to('.box',{
   top: '100%',
   right:'50%',
+  rotation:180,
   scale:0.6,
 })
 
-tl.to('.box',{
+var tl3 = gsap.timeline({
   scrollTrigger:{
-    trigger:'.section-2',
-    start:'50% 50%',
-    end:'120% 40%',
+    trigger:'.section-3',
+    start:'10% 90%',
+    end:'50% 60%',
     scrub:true,
-  },
+  }
+})
+
+tl3.to('.box',{
   top: '185%',
   right:'25%',
+  rotate:'120deg',
   scale: 0.5,
 })
 
 
-tl.to('.card-img img',{
-    scrollTrigger: {
-      trigger: '.section-3',
-      start: '20% 50%',
-      end: '40% 50%%',
-      scrub:true
-    },
+tl3.to('.card-img img',{
     top:'-10%',
     ease: 'power2.out',
   })
